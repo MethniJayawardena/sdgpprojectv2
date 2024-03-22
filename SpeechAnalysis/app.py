0c65deedb1eb7f924b58b21e8238c462a0e0f4b7
SanuthMandepa <dssanuthmandepa@gmail.com>
2024-03-23 02:05:43 +0530

    connect with flask

--- SpeechAnalysis/app.py
+++ SpeechAnalysis/app.py
@@ -1,42 +1,33 @@
-from flask import Flask, request, jsonify,render_template
-import joblib
+from flask import Flask, render_template, request, jsonify
+import librosa
 import numpy as np
+from joblib import load
 
-app = Flask(__name__,template_folder='templates')
+app = Flask(__name__)
 
-# Load the pickled model
-model = joblib.load('./Emotion_Voice_Detection_Model.pkl')
+# Load the model and encoder
+model = load('./emotion_detection_model.joblib')
+enc = load('./label_encoder.joblib')
 
-
+# Function to extract MFCC features
+def extract_mfcc(audio_file_path):
+    data, sampling_rate = librosa.load(audio_file_path)
+    mfcc_features = librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40)
+    return np.mean(mfcc_features.T, axis=0)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
-    #     template_path = 'index.html'
-    #     print("Attempting to render template:", template_path)
-        
-    # except Exception as e:
-    #     print("Error:", str(e))
-    #     return "An error occurred while rendering the template."
+    return render_template('index.html')
 
-
-
-
-# Define a route for prediction
-@app.route('/api/predict', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    # Extract JSON data from the POST request
-    data = request.json
-    
-#     # Preprocess the data if needed
-#     # Example: Convert JSON data to numpy array
-    features = np.array(data['features'])
-    
-    # Make predictions using the loaded model
-    prediction = model.predict(features.reshape(1, -1))
-    
-#     # Return the prediction as a JSON response
-    return jsonify({'prediction': prediction[0]})
+    audio_file = request.files['audio']
+    audio_file.save('uploaded_audio.wav')
+    input_features = extract_mfcc('uploaded_audio.wav')
+    input_features = input_features.reshape(1, -1)
+    predictions = model.predict(input_features)
+    predicted_emotion = enc.inverse_transform(predictions)
+    return jsonify({'predicted_emotion': predicted_emotion[0]})
 
 if __name__ == '__main__':
     app.run(debug=True)
