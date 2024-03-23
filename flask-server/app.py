3778560e47f8d1a449af171a179f55389cbef642
Shehan Fernando <shehannavindu30@gmail.com>
2024-03-23 15:53:24 +0530

    coding update

--- flask-server/app.py
+++ flask-server/app.py
@@ -11,14 +11,17 @@ app = Flask(__name__)
 # Route for seeing a data
 @app.route('/data')
 def get_time():
- 
+     
     # Returning an api for showing in  reactjs
-    return {
+     return {
         'Name':"geek", 
         "Age":"22",
         "Date":x, 
         "programming":"python"
         }
+ 
+
+
  
      
 # Running app
